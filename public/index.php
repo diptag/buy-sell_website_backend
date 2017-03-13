a8e82404d79ee600b8f01d2198826ec927620594
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-13 11:51:44 +0000

    Created index.php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+    // configuration
+    require("../controllers/includes/config.php");
+
+    // check which controller to use
+    if (isset($_GET["url"]))
+        $url = $_GET["url"];
+    else
+        $url = "main";
+
+    // use the required controller
+    require("../controllers/".$url.".php");
+?>
