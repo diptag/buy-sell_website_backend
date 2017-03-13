d0155d670ad0b28e44b1d0d864af67de68094960
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-13 17:46:50 +0000

    Modified config.php and helpers.php

--- controllers/includes/helpers.php
+++ controllers/includes/helpers.php
@@ -3,5 +3,16 @@
      * File containing helpful functions for the website
      */
 
-    
+    // render view passed as value
+    function render ($view, $values = [])
+    {
+        // extract variables in local scope
+        extract($values);
+
+        // render views between header and footer
+        require(__DIR__."/../../views/header.php");
+        require(__DIR__."/../../views/".$view.".php");
+        require(__DIR__."/../../views/footer.php");
+        exit;
+    }
 ?>
