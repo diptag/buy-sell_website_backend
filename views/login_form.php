92a09c16771b01402d34236742d7c89c2c82de9d
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-21 19:33:03 +0000

    Created login_form.php

---
+++ views/login_form.php
@@ -0,0 +1,5 @@
+<form action="login" method="POST">
+    <input type="email" placeholder="Enter Email" name="email"><br>
+    <input type="password" placeholder="Enter Password" name="password"><br>
+    <input type="submit" value="Log In">
+</form>
