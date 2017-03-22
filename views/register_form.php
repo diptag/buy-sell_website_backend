0b5c4bb8e314c371135955fde72dc6c9008439ca
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-22 16:20:27 +0000

    Created register_form.php

---
+++ views/register_form.php
@@ -0,0 +1,9 @@
+<form action="register" method="POST">
+    <input type="text" placeholder="Enter Name" name="name"><br>
+    <input type="email" placeholder="Enter Email" name="email"><br>
+    <select name="college">
+    </select><br>
+    <input type="password" placeholder="Enter Password" name="password"><br>
+    <input type="password" placeholder="Re - Enter Password" name="password_2"><br>
+    <input type="submit" value="Log In">
+</form>
