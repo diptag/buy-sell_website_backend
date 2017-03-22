7cd8a20d3e8c7f38cd34cb828e3b5f5d6e2a5d2a
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-22 19:46:04 +0000

    Added Error handling messages to login_form.php

--- views/login_form.php
+++ views/login_form.php
@@ -1,4 +1,12 @@
 <form action="login" method="POST">
+    <?php
+        if (isset($error_msg))
+        {
+    ?>
+    <div id="error-msg"><?= $error_msg ?></div>
+    <?php
+        }
+    ?>
     <input type="email" placeholder="Enter Email" name="email"><br>
     <input type="password" placeholder="Enter Password" name="password"><br>
     <input type="submit" value="Log In">
