1e7e793ad537232ea975fb4d1a6da34d5e2a09ae
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-22 13:01:04 +0000

    Added login code to login.php

--- controllers/login.php
+++ controllers/login.php
@@ -1,3 +1,41 @@
 <?php
-    print_r($_GET);
+
+    // if user reached the page via GET (i.e. by clicking a linlk orredirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        render("login_form", ["title" => "Log In"]);
+    } 
+    
+    // id the user reached the page via POST (i.e. by submitting a form)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if (!isset($_POST["email"]) || !isset($_POST["password"]))
+        {
+            render("login_form", ["title" => "Log In", "error_msg" => "Invalid email or Password"]);
+        }
+        
+        // query database for the user
+        $user = get_usr($_POST["email"]);
+        
+        // if user doesn't exists in database
+        if (count($user) == 1)
+        {
+            // check if password is correct
+            if (password_verify($_POST ["password"], $user["hash"]))
+            {
+                // remember that user is logged in by upadating user's id in session
+                $_SESSION["id"] = $user["id"];
+                
+                // redirect to main.php
+                redirect("/");
+            }
+        }
+        
+        // id user doesn't exists display error message
+        else
+        {
+            render("login_form", ["title" => "Log In", "error_msg" => "User doesn't exists"]);
+        }
+    }
+    
 ?>
