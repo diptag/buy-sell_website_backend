2c10d4500683b22060f17371d566aa0f414d3c44
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-24 17:50:13 +0000

    Added store_view.php

---
+++ views/store_view.php
@@ -0,0 +1,12 @@
+<form action="search.php">
+    <input type="search" name="q" placeholder="Search">
+    <label>Search by:</label>
+    <select name="category" placeholder="Category">
+        <option value="category">Category</option>
+        <option value="college">College</option>
+    </select>
+    <input type="submit" value="Search">      
+</form>
+<div id="items-display">
+    <div id="categories"></div>
+</div>
