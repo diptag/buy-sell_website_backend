58332c8070f022db31c292e758164205029eae35
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-26 17:54:46 +0000

    Deleted sell_view.ph

--- views/sell_view.php
+++
@@ -1,8 +0,0 @@
-<form action="sell" method="POST" enctype="multipart/form-data">
-    Name: <input type="text" name="name"><br>
-	Description: <textarea cols="30" rows="5" maxlength="255" name="description"></textarea><br>
-    Category: <select name="category"></select><br>
-    Price: Rs.<input type="number" name="price"><br>
-    Image: (Image should be of JPEG, JPG, PNG and GIF formats and size &lt; 1MB)<br><input type="file" name="image_upload" accept="image/jpeg, image/gif, image/x-png"><br>
-    <input type="submit">           
-</form>
