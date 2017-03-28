83b3ea6013eb951c9e7f24ea34f24a047dbd80f0
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-28 18:28:03 +0000

    Added navigation bar to header.php

--- views/header.php
+++ views/header.php
@@ -9,5 +9,8 @@
 <body>
     <div id="wrapper">
         <header>
+        	<div class="nav-options"><a href="store">Go to Store</a></div>
+            <div class="nav-options"><a href="sell">Sell a Product</a></div>
+            <div class="nav-options"><a href="login">Login</a> or <a href="register">Register</a></div>
         </header>
         <div id="main">
