9090dc89349494ec999ce6ccc3c5ebadb3256bcf
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-28 18:27:27 +0000

    Debugged the code for redirection if user is not logged in

--- includes/config.php
+++ includes/config.php
@@ -9,11 +9,11 @@
     // require authetication for all pages except the pages in the array
     if (empty($_SESSION["id"]))
     {
-        if (in_array($_GET["page"], ["sell", "profile", "change_password", "logout", "buy"]))
+        if (in_array($_GET["url"], array("sell", "profile", "change_password", "logout")))
         {
             // store msg to be displayed in $_SESSION
-            $_SESSION["error_msg"] = "You need to login first.";
-            $_SESSION["redirecting_page"] = $_GET["page"];
+            $_SESSION["login_msg"] = "You need to login first.";
+            $_SESSION["redirecting_page"] = $_GET["url"];
             redirect("/login");
         }
     }
