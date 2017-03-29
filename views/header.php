98f62070e9e8f29d7337ebde27b19f2354520e9c
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-29 14:04:16 +0000

    Added a dropdown menu to nav bar

--- views/header.php
+++ views/header.php
@@ -9,8 +9,32 @@
 <body>
     <div id="wrapper">
         <header>
+            <?php 
+                // check if user is logged in
+                if (!empty($_SESSION["name"]))
+                {
+            ?>
+        	        <div id="dropdown">
+            	        <div id="dropdown-btn">Hi! <?= $_SESSION["name"] ?></div>
+            	        <div id="dropdown-options">
+                	        <a href="profile">My products</a>
+                            <a href="change_password">Change Password</a>
+                            <a href="logout">Log Out</a>
+                        </div>
+                    </div>
+            <?php
+                }
+            ?>
         	<div class="nav-options"><a href="store">Go to Store</a></div>
             <div class="nav-options"><a href="sell">Sell a Product</a></div>
-            <div class="nav-options"><a href="login">Login</a> or <a href="register">Register</a></div>
+            <?php
+                // check if user is logged in
+                if (empty($_SESSION["id"]))
+                {
+            ?>
+                    <div class="nav-options"><a href="login">Login</a> or <a href="register">Register</a></div>
+            <?php
+                }
+            ?>
         </header>
         <div id="main">
